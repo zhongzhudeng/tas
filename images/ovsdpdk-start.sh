1aacc0eb31f1fca68e83f5b04b6775dabab0b5d4
stolet <matheusstolet@hotmail.com>
2023-07-27 11:59:11 +0200

    set cpu affinity for ovs and ovs dpdk

--- images/ovsdpdk-start.sh
+++ images/ovsdpdk-start.sh
@@ -8,8 +8,12 @@ ovs-ctl --no-ovs-vswitchd start
 
 echo "Setting dpdk-init to true"
 ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
+echo "Setting dpdk mask"
+ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-lcore-mask=0x2a
+echo "Setting hugepages dir"
+ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-hugepages-dir=/dev/hugepages
 echo "Setting cpu mask"
-ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=0xa
+ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=0xa80
 
 echo "Starting ovs switch"
 ovs-ctl --no-ovsdb-server --db-sock="$DB_SOCK" start
