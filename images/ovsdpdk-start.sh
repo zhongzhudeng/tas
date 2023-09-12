088154700f7b07ec2657db5dc37d81ec87df8b02
stolet <matheusstolet@hotmail.com>
2023-09-13 00:48:42 +0200

    add pmd mask to ovsdpdk-start script

--- images/ovsdpdk-start.sh
+++ images/ovsdpdk-start.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+pmd_mask=$1
 
 export PATH=$PATH:/usr/local/share/openvswitch/scripts
 export DB_SOCK=/usr/local/var/run/openvswitch/db.sock
@@ -11,7 +12,7 @@ ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-init=true
 echo "Setting hugepages dir"
 ovs-vsctl --no-wait set Open_vSwitch . other_config:dpdk-hugepages-dir=/dev/hugepages
 echo "Setting cpu mask"
-ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=0x55555
+ovs-vsctl --no-wait set Open_vSwitch . other_config:pmd-cpu-mask=$pmd_mask
 
 echo "Starting ovs switch"
 ovs-ctl --no-ovsdb-server --db-sock="$DB_SOCK" start
