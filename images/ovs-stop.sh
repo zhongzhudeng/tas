161facca70e4a29706a50ff043c162f5a59ad578
stolet <matheusstolet@hotmail.com>
2024-02-13 14:15:10 +0100

    removing experiment and images directory

--- images/ovs-stop.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-/usr/local/share/openvswitch/scripts/ovs-ctl stop
--- images/ovsdpdk-stop.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-
-killall ovsdb-server ovs-vswitchd
-rm -f /var/run/openvswitch/vhost-user*
-rm -f /usr/local/etc/openvswitch/conf.db
